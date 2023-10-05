8f3268f265998cc2661c677c948ce74335ec5123
RickySBahaso <ricky@sali-evo.com>
2023-10-05 23:34:33 +0700

    nav bar and useQuery

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,12 @@
+import Nav from '../../components/Nav'
+
+export default function Cart() {
+    return (
+      <>
+        <Nav />
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        Cart Page!
+        </main>
+      </>
+    )
+  }
